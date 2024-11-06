7a78414d3e734ba7e20815aca0ea5de15656b54c
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-11-06 22:20:38 +0700

    edit winston

--- src/infrastructure/utils/winston.ts
+++ src/infrastructure/utils/winston.ts
@@ -3,7 +3,6 @@ import winston from "winston";
 export const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
